2a8c1f116d6b86a177761f1c0c8113ac14c2fa7c
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-07 23:40:01 -0300

    init

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex w-screen h-screen flex-col items-center justify-center">
-      <h1 className="flex flex-row items-center text-7xl font-extrabold text-blue-500">REXANTEC</h1>
-      <p className="flex flex-row items-center mt-10 font-semibold">En construcción... Proximamente.</p>
-      <Link className="mt-8 text-blue-300 hover:text-blue-600 hover:font-semibold" href={"tomail:consultas@rexantecservicios.com.ar"}>Contactanos</Link>
+    <main className="flex w-screen h-screen flex-col items-center justify-center select-none">
+      <h1 className="flex flex-row items-center text-9xl font-extrabold text-blue-500/50 skew-y-12 transform delay-1000 hover:text-blue-500/95 cursor-default">REXANTEC</h1>
+      <p className="flex flex-row items-center mt-20 font-semibold">En construcción... Próximamente.</p>
+      <Link className="mt-2 text-blue-500 font-semibold hover:text-blue-500/95 hover:font-medium hover:underline italic" href={"mailto:consultas@rexantecservicios.com.ar"}>Contáctenos</Link>
     </main>
   );
 }
