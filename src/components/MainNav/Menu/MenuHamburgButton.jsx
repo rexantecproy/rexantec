2c6cdfb6ae486f28014cf2f7f232f1dd11e47032
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-11 23:36:49 -0300

    dropbox hamburg

--- src/components/MainNav/Menu/MenuHamburgButton.jsx
+++ src/components/MainNav/Menu/MenuHamburgButton.jsx
@@ -7,16 +7,16 @@ export default function MenuHamburgButton({ props }) {
     const [isOpenMenu, setIsOpenMenu] = useState(false);
     return (
         <>
-            <ul className={`md:hidden basis-0 flex justify-center content-center cursor-pointer p-3 hover:rounded hover:border-zinc-600 hover:bg-slate-100 hover:shadow-xl active:shadow-2xl ${isOpenMenu ? 'activeMenuHamburg' : ''} hover:text-red-800`}>
+            <ul className={`md:hidden basis-0 flex justify-center content-center cursor-pointer p-3 hover:rounded hover:border-sky-700 hover:bg-sky-100 hover:shadow-xl active:shadow-2xl ${isOpenMenu ? 'activeMenuHamburg' : ''} hover:text-red-800`}>
                 <span onClick={() => { setIsOpenMenu(!isOpenMenu) }}>
                     {
                         isOpenMenu ? Close : MenuIcon
                     }
                 </span>
             </ul>
-            <nav className={`h-screen md:hidden bg-black/0 w-screen fixed top-16 left-0 z-auto flex justify-center ${isOpenMenu ? '' : 'hidden'}`}>
-                <div className='absolute z-[4] w-full h-full bg-black/10' onClick={() => { setIsOpenMenu(!isOpenMenu) }}></div>
-                <div className={`z-[5] absolute w-72 h-80 bg-white/90 justify-center flex bg-border-black/0 border-b-4 border-pink-500 `}>
+            <nav className={`h-screen md:hidden bg-sky-500/0 w-screen fixed top-16 left-0 z-auto flex justify-center ${isOpenMenu ? '' : 'hidden'}`}>
+                <div className='absolute z-[4] w-full h-full bg-sky-500/10' onClick={() => { setIsOpenMenu(!isOpenMenu) }}></div>
+                <div className={`z-[5] absolute w-72 h-80 bg-sky-100/90 justify-center flex bg-border-black/0 border-b-4 border-sky-500 `}>
                     <List props={props} />
                 </div>
             </nav>
