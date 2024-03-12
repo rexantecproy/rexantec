2a89cb99fa38b624c687d87bed57915ece07f680
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-11 22:00:45 -0300

    logo nav

--- src/components/MainNav/List/ListNoSvg.jsx
+++ src/components/MainNav/List/ListNoSvg.jsx
@@ -4,14 +4,14 @@ import MenuHamburgButton from '../Menu/MenuHamburgButton';
 
 export default function ListNoSvg({ props }) {
     return <>
-        <ul className="z-[-1] md:z-auto md:static absolute bg-transparent w-full left-0 py-4 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 md:flex md:flex-grow md:justify-center basis-0 md:text-sm md:[&>li>a]:inline-block md:[&>li>a]:p-2 [&>li>a]:text-sky-100 select-none nth-7:hidden">
+        <ul className="z-[-1] md:z-auto md:static absolute bg-transparent w-full left-0 py-4 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 md:flex md:flex-grow md:justify-center basis-0 md:text-sm md:[&>li>a]:inline-block md:[&>li>a]:p-2 [&>li>a]:text-sky-100 select-none nth-1:rounded-l-lg nth-3:rounded-r-lg">
             {
                 props.map((categorias, i) => {
                     return (
                         categorias.name == 'Catálogo' ?
                             (<GlobalList key={`$ID-${i}DROP`} sub={categorias.list} title={categorias.name} />)
                             :
-                            (<li key={`$ID-${i}`} className='border border-transparent bg-sky-600 mx-1 box-border hover:border hover:border-lime-600 rounded-md [&>a]:activate:bg-sky-500 hover:bg-lime-500 hover:shadow-xl active:shadow-2xl'>
+                            (<li key={`$ID-${i}`} className='border border-transparent bg-sky-600 box-border hover:border hover:border-lime-600  [&>a]:activate:bg-sky-500 hover:bg-lime-500 hover:shadow-xl active:shadow-2xl'>
                                 <Link href={categorias.linknav}>{categorias.name}</Link>
                             </li>)
                     )
--- src/components/MainNav/NavBar.jsx
+++ src/components/MainNav/NavBar.jsx
@@ -3,12 +3,14 @@ import LISTNAV from '@/BBDD/LISTNAV.json'
 import ListNoSvg from './List/ListNoSvg.jsx';
 //mport ListSvg from "./List/ListSvg.jsx";
 import TitleLogo from '../Utilities/TitleLogo.jsx';
+import Image from 'next/image.js';
 
 
 export default function NavBar () {
     return <header className='divide-y-2 divide-y-reverse divide-sky-800 w-3/5 '>
         {/* <Contact /> */}
         <nav className="py-2 px-10 h-auto md:h-20 flex items-center fixed top-0 md:top-10 w-3/5 justify-between bg-sky-600/10"> {/* aca le quito un z-[100] */}
+            <Image src={"https://i.imgur.com/ZoO6k92.png"} width={50} height={50}/>
             <TitleLogo component={'nav'} title={'Rexantec'} />
             <ListNoSvg props={LISTNAV} />
             {/* <ListSvg /> */}
