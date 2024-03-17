4d215712797c6fe19e91deb693c477a37a588781
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-17 13:48:37 -0300

    refactor footer responsive

--- src/components/MainFooter/Footer.jsx
+++ src/components/MainFooter/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer () {
         <div className="w-full flex justify-center text-sky-600 bg-gradient-to-t from-sky-50 from-10% via-sky-200 via-90% to-sky-400 to-100% select-none">
             <div className="flex flex-col w-3/5 justify-center">
                 <div className="flex flex-row items-center md:items-start mt-10 mb-5 nth-4:mr-10 [&_ul]:ml-4 md:[&>ul]:h-44">
-                    <ul className="flex flex-col w-2/12 items-center divide-x-reverse divide-x-2 divide-solid divide-sky-800 pointer-events-none">
+                    <ul className="flex flex-col md:w-2/12 items-center divide-x-reverse divide-x-2 divide-solid divide-sky-800 pointer-events-none">
                         <Image src={"https://i.imgur.com/ZoO6k92.png"} width={100} height={100} alt="Logo Footer"/>
                     </ul>
                     <ul className="hidden md:flex flex-col w-2/12 nth-1:mb-2 nth-1:text-center [&>li]:py-1 [&>li]:text-xs nth-1:text-base divide-x-reverse divide-x-2 divide-solid divide-sky-800">
@@ -43,22 +43,32 @@ export default function Footer () {
                             <Link className='hover:hover:text-sky-800' href={"/"}>&nbsp;&nbsp;Conocé sobre la historia de Rexantec en el mundo del Aire Acondicionado.</Link>
                         </li>
                     </ul>
-                    <ul className="md:w-4/12 text-end">
-                        <li>ATENCIÓN AL CLIENTE</li>
+                    <ul className="md:w-4/12 w-2/5 text-end md:text-base">
+                        <li className="text-xs md:text-base">ATENCIÓN AL CLIENTE</li>
                         <li className="my-2"><HrComponent color={"#075985"} height={3} /></li>
-                        <li className="md:text-xl mb-4 select-text">
+                        <li className="md:text-xl text-sm mb-4 select-text">
                             <Link className='hover:text-sky-800' href={"tel:+541158947428"}>(+54) 9 1158947428</Link>
                         </li>
-                        <li className="text-sky-800">COTIZACIONES</li>
+                        <li className="text-sky-800 text-xs md:text-base">COTIZACIONES</li>
                         <li className="my-2"><HrComponent color={"#075985"} height={3} /></li>
                         <li className="md:text-lg select-text">
                         <Link className='hover:text-sky-800' href={"tel:+541158947428"}>(+54) 9 1158947428</Link>
                         </li>
                     </ul>
                 </div>
-                <div className="flex flex-col">      
+
+                <div className="flex flex-col">  
+                    <HrComponent color={"#075985"} height={3} />  
+                    <div className="flex flex-col my-4 md:hidden [&_a]:text-xs [&_a]:my-1 [&_a]:hover:text-sky-800">
+                        <p className="text-center text-sm">REDES SOCIALES</p>
+                        <Link href={"mailto:informes@rexantecservicios.com.ar"}>Email</Link>
+                        <Link href={"https://www.facebook.com/profile.php?id=100083022592038"}>Facebook</Link>
+                        <Link href={"https://www.instagram.com/rexantec_servicios/"}>Instagram</Link>
+                        <Link href={"https://www.tiktok.com/@rexantec?_t=8kjKop3NavG&_r=1"}>Tik Tok</Link>
+                        <Link href={"https://wa.me/5491158947428?text=Realice%20su%20consulta%20sobre%20todos%20nuestros%20servicios%20y%20productos%20directamente.%20EQUIPO%20REXANTEC."}>Whatsapp</Link>
+                    </div>  
                     <HrComponent color={"#075985"} height={3} />                   
-                    <div className="flex flex-row w-full justify-between pb-5 pt-2 md:text-base text-xs">
+                    <div className="flex flex-col md:flex-row w-full justify-between pb-5 pt-2 md:text-base text-xs">
                         <p>© Copyright | Rexantec 2024</p>
                         <p>Hecho por <Link href={'https//:wwww.google.com.ar'} className="hover:font-semibold hover:text-sky-800 pl-1 hover:pl-0.5">Maidana Iván</Link></p>
                     </div>
