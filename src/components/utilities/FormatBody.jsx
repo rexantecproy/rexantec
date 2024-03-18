7dc2f0c2b59b72b0f92147560d03b7869e5b344f
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-18 15:48:33 -0300

    Refactos pages.js

--- src/components/utilities/FormatBody.jsx
+++ src/components/utilities/FormatBody.jsx
@@ -8,7 +8,7 @@ export default function Format({ children, Title }) {
             <main className="flex h-screen flex-col items-center justify-center select-none">
                 <h1 h1 className="flex flex-row text-center sm:text-8xl text-3xl font-extrabold bg-gradient-to-r from-sky-500/5 from-0% to-sky-500 to-100%  hover:bg-gradient-to-r hover:from-red-500/5 hover:from-0% hover:to-red-500 hover:to-100% text-black cursor-default p-2">
                     En Desarrollo: {titlePageStr} 
-                </h1>
+                 </h1> {/* Esto se comenta cuando ya no este en desarrollo. */}
                 {children}
             </main>
         </div>
