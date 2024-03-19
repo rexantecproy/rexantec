2fb00d80696a18d113f1ee150faf840e12c48eaa
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-18 23:26:51 -0300

    refators LISTNAV

--- src/components/MainNav/DropDown/List/List1.jsx
+++ src/components/MainNav/DropDown/List/List1.jsx
@@ -7,13 +7,7 @@ export default function SubList1({ sub_1 }) {
             sub_1.length > 0 && sub_1.map((item, it) => {
                 return (
                     <li key={`${item.name}-${it}`} className="w-full relative hover:bg-sky-100 hover:text-sky-500 flex flex-row justify-center group/list_next hover:flex">
-                        {
-                            item.sub_content_1.length == 0
-                                ?
-                                (<Link href={item.href}>{item.name}</Link>)
-                                :
-                                (<span>{item.name}</span>)
-                        }
+                        <Link href={item.href}>{item.name}</Link>
                         <SubList2 sub_2={item} />
                     </li>
                 )
--- src/components/MainNav/DropDown/List/SubList1.jsx
+++ src/components/MainNav/DropDown/List/SubList1.jsx
@@ -5,7 +5,7 @@ export default function SubList2({ sub_2 }) {
     return <>
         {
             sub_2.sub_content_1.length > 0 && <div className='absolute right-2 invisible group-hover/list_next:visible group/list_2 hover:flex'>
-                <Next className="w-5 h-5 hover:border hover:border-sky-500/50 hover:rounded-full " />
+                <Next className="w-5 h-5 hover:border hover:border-sky-500/50 hover:rounded-full" />
                 <ul key={`${sub_2.name}-`} className='absolute hidden ml-4 group-hover/list_2:flex flex-col w-44 justify-center bg-sky-500/75 list-none select-none [&>li]:text-sky-100 [&>li]:py-3 [&>li]:px-4 rounded-b-lg border border-sky-500/75 [&_li]:last:hover:rounded-b-lg [&_li]:nth-1:hover:rounded-b-lg'>
                     {
                         sub_2.sub_content_1.length > 0 && sub_2.sub_content_1.map((item_2, i) => {
