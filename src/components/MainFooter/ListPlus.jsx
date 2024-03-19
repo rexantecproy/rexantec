8696314056b999cb65a10d7bc33bf402ea417a68
Maidana Ivan <ivan.maidana99122@gmail.com>
2024-03-19 00:09:40 -0300

    refactors

---
+++ src/components/MainFooter/ListPlus.jsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import LISTNAV2 from '../../BBDD/LISTNAV';
+import EMAILS from '../../BBDD/EMAILS';
+
+export default function ListPlus() {
+    return (
+        <>
+            <ul className="flex flex-col md:w-2/12 items-center divide-x-reverse divide-x-2 divide-solid divide-sky-800 pointer-events-none">
+                <Image src={"https://i.imgur.com/ZoO6k92.png"} width={100} height={100} alt="Logo Footer" />
+            </ul>
+            <ul className="hidden md:flex flex-col w-2/12 nth-1:mb-2 nth-1:text-center [&>li]:py-1 [&>li]:text-xs nth-1:text-base divide-x-reverse divide-x-2 divide-solid divide-sky-800">
+                <li>{LISTNAV2[0].name.toUpperCase()}</li>
+                <li>
+                    <Link className='hover:font-semibold hover:text-sky-800' href={EMAILS[0].link}>{EMAILS[0].name}</Link>
+                </li>
+                <li>
+                    <Link className='hover:font-semibold hover:text-sky-800' href={EMAILS[1].link}>{EMAILS[1].name}</Link>
+                </li>
+                <li>
+                    <Link className='hover:font-semibold hover:text-sky-800' href={EMAILS[2].link}>{EMAILS[2].name}</Link>
+                </li>
+            </ul>
+            <ul className="hidden md:flex flex-col w-2/12 nth-1:mb-2 nth-1:text-center [&>li]:py-1 [&>li]:text-xs nth-1:text-base divide-x-reverse divide-x-2 divide-solid divide-sky-800">
+                <li>{LISTNAV2[1].name.toUpperCase()}</li>
+                {
+                    LISTNAV2[1].list.map((listNav, i) => {
+                        return (<li key={`${listNav}--${i}Ñ`}>
+                            <Link key={`${listNav}--${i}Ñ`} className={`hover:font-semibold hover:text-sky-800`} href={listNav.href}>{`${listNav.name.charAt(0).toUpperCase()}${listNav.name.slice(1).toLowerCase()}`}</Link>
+                        </li>)
+                    })
+                }
+            </ul>
+            <ul className="hidden md:flex flex-col nth-1:mb-2 nth-1:text-center nth-2:text-xs w-3/12 [&>li]:py-1 divide-x-reverse divide-x-2 divide-solid divide-sky-800">
+                <li>{LISTNAV2[2].name.toUpperCase()}</li>
+                <li>
+                    <Link className='hover:hover:text-sky-800' href={LISTNAV2[1].list[0].href}>&nbsp;&nbsp;Conocé sobre la historia de Rexantec en el mundo del Aire Acondicionado.</Link>
+                </li>
+            </ul>
+            <ul className="md:w-4/12 w-2/5 text-end">
+                <li className="text-xs md:text-base">ATENCIÓN AL CLIENTE</li>
+                <li className="my-2"><hr className="bg-sky-800 h-[3px]" /></li>
+                <li className="md:text-xl text-sm mb-4 select-text">
+                    <Link className='hover:text-sky-800' href={"tel:+541158947428"}>(+54) 9 1158947428</Link>
+                </li>
+                <li className="text-sky-800 text-xs md:text-base">COTIZACIONES</li>
+                <li className="my-2"><hr className="bg-sky-800 h-[3px]" /></li>
+                <li className="md:text-lg select-text">
+                    <Link className='hover:text-sky-800' href={"tel:+541158947428"}>(+54) 9 1158947428</Link>
+                </li>
+            </ul>
+        </>
+    )
+};
